0152b416b37041774b8574e020b83ca215466fc6
Annyce Davis <annyce.davis@gmail.com>
2025-01-03 15:32:53 -0500

    [+]: Added chapter one challenge and solution
    * Added correct fonts to the view layout

---
+++ app/src/main/java/com/example/sample/0108Solution.kt
@@ -0,0 +1,79 @@
+package com.example.sample
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.red30.R
+import com.example.red30.ui.theme.Red30TechTheme
+
+@Composable
+fun Solution0108(modifier: Modifier = Modifier) {
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        shape = RoundedCornerShape(0.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier.padding(4.dp)
+                        .size(90.dp),
+                    painter = painterResource(R.mipmap.ic_launcher_foreground),
+                    contentDescription = stringResource(R.string.logo)
+                )
+                Text(
+                    text = stringResource(R.string.tagline),
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.headlineLarge,
+                )
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+            Text(
+                text = stringResource(R.string.about_header_label),
+                style = MaterialTheme.typography.titleLarge,
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.about_content),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Solution0108Preview() {
+    Red30TechTheme {
+        Solution0108()
+    }
+}
