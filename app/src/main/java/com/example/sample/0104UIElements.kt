e639ac1706efcc782b2da87c8eb069ffce7d3b15
Annyce Davis <annyce.davis@gmail.com>
2025-02-05 14:58:51 -0500

    [*]: 01_04 end

--- app/src/main/java/com/example/sample/0104UIElements.kt
+++ app/src/main/java/com/example/sample/0104UIElements.kt
@@ -1,18 +1,24 @@
 package com.example.sample
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Greeting0104(
     modifier: Modifier = Modifier,
     name: String
 ) {
-    Column {
+    Box(
+        modifier = modifier.height(400.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
         Text(
             modifier = modifier,
             text = "Hello $name!",
