98c2802cf0c9cbc709b184ed91d26df53ca2893f
Annyce Davis <annyce.davis@gmail.com>
2025-02-05 14:59:25 -0500

    [+]: 01_05 start

---
+++ app/src/main/java/com/example/sample/0105Measurement.kt
@@ -0,0 +1,39 @@
+package com.example.sample
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Greeting0105(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Having fun with Compose?",
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.NEXUS_5,
+    name = "Nexus 5"
+)
+@Composable
+private fun Greeting0104Preview() {
+    Greeting0105()
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_4_XL,
+    name = "Pixel 4 XL"
+)
+@Composable
+private fun Greeting0104PixelPreview() {
+    Greeting0105()
+}
