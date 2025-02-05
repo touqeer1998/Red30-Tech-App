4e9bb142060593d01fba7e68a1cf9924714a6169
Annyce Davis <annyce.davis@gmail.com>
2025-02-05 17:01:28 -0500

    [*]: 01_08 solution

--- app/src/main/java/com/example/sample/0108Solution.kt
+++ app/src/main/java/com/example/sample/0108Solution.kt
@@ -3,14 +3,18 @@ package com.example.sample
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,23 +28,43 @@ import com.example.red30.compose.ui.theme.Red30TechTheme
 fun Solution0108(modifier: Modifier = Modifier) {
     ElevatedCard(
         modifier = modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(16.dp),
         shape = RoundedCornerShape(0.dp)
     ) {
         Column(modifier = Modifier.fillMaxWidth()) {
-            Row(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Image(
-                    modifier = Modifier.padding(4.dp),
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .size(90.dp),
                     painter = painterResource(R.mipmap.ic_launcher_foreground),
                     contentDescription = stringResource(R.string.logo)
                 )
                 Text(
-                    text = "TODO",
+                    text = stringResource(R.string.tagline),
                     textAlign = TextAlign.Center,
                     color = MaterialTheme.colorScheme.secondary,
                     style = MaterialTheme.typography.headlineLarge,
                 )
             }
+
+            Spacer(Modifier.height(24.dp))
+
+            Text(
+                text = stringResource(R.string.about_header_label),
+                style = MaterialTheme.typography.titleLarge,
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.about_content),
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
