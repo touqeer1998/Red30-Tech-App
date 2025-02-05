dbbaa1b7a030c968dfe0f83ef8696946c30053af
Annyce Davis <annyce.davis@gmail.com>
2025-02-05 16:42:42 -0500

    [*]: 01_06 end

--- app/src/main/java/com/example/sample/0106Modifiers.kt
+++ app/src/main/java/com/example/sample/0106Modifiers.kt
@@ -1,9 +1,11 @@
 package com.example.sample
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,6 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,11 +34,14 @@ fun Modifiers0106(modifier: Modifier = Modifier) {
                 Box(
                     modifier = Modifier
                         .height(50.dp)
+                        .weight(1f)
+                        .padding(top = 4.dp, end = 4.dp)
                         .background(MaterialTheme.colorScheme.secondary)
                 )
                 Box(
                     modifier = Modifier
                         .height(50.dp)
+                        .weight(1f)
                         .padding(top = 4.dp)
                         .background(MaterialTheme.colorScheme.tertiary)
                 )
@@ -44,16 +50,20 @@ fun Modifiers0106(modifier: Modifier = Modifier) {
                         .width(50.dp)
                         .height(50.dp)
                         .padding(top = 4.dp)
+                        .clickable(onClick = {})
                         .background(MaterialTheme.colorScheme.primary)
                 ) {
                     Box(
                         modifier = Modifier
                             .width(24.dp)
                             .height(24.dp)
+                            .align(Alignment.TopCenter)
                             .background(MaterialTheme.colorScheme.background)
                     )
                 }
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
 
             Box(
                 modifier = Modifier
@@ -61,6 +71,8 @@ fun Modifiers0106(modifier: Modifier = Modifier) {
                     .height(200.dp)
                     .background(MaterialTheme.colorScheme.error)
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
 
             Box(
                 modifier = Modifier
