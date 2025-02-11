00fa32bea6e3815361607be73b429567b11b1f4d
Annyce Davis <annyce.davis@gmail.com>
2025-02-11 09:33:15 -0500

    [+]: 03_02 start

---
+++ app/src/main/java/com/example/sample/0302State.kt
@@ -0,0 +1,51 @@
+package com.example.sample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.red30.compose.ui.theme.Red30TechTheme
+
+@Composable
+fun State0302(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(
+            space = 48.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        var checked = true
+
+        Switch(
+            checked = checked,
+            onCheckedChange = { checked = !checked }
+        )
+
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = !checked }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun State0302Preview() {
+    Red30TechTheme {
+        Surface {
+            State0302()
+        }
+    }
+}
