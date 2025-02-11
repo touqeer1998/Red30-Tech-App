0b2a03bee5e30fbac7a26aed9420f1a98541713b
Annyce Davis <annyce.davis@gmail.com>
2025-02-11 18:47:54 -0500

    [*]:; 04_02 end

--- app/src/main/java/com/example/sample/0402Koin.kt
+++ app/src/main/java/com/example/sample/0402Koin.kt
@@ -3,14 +3,23 @@ package com.example.sample
 import androidx.lifecycle.ViewModel
 
 // our class that uses a dependency
-class SampleViewModel: ViewModel() {
-    val repository: DefaultSampleRepository = DefaultSampleRepository()
-
+class SampleViewModel(repository: SampleRepository ): ViewModel() {
     init {
         repository.doSomething()
     }
 }
 
-class DefaultSampleRepository() {
-    fun doSomething() {}
+interface SampleRepository {
+    fun doSomething()
 }
+
+class DefaultSampleRepository() : SampleRepository {
+    override fun doSomething() {}
+}
+
+class FakeSampleRepository() : SampleRepository {
+    override fun doSomething() {}
+}
+
+val viewModel = SampleViewModel(DefaultSampleRepository())
+val viewModelInTests = SampleViewModel(FakeSampleRepository())
