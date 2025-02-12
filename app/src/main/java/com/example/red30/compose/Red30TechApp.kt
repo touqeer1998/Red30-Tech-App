dde42ed0deb7e89cd3916a2fcee8cbdbe506a092
Annyce Davis <annyce.davis@gmail.com>
2025-02-11 19:05:32 -0500

    [+]: 04_03 start

--- app/src/main/java/com/example/red30/compose/Red30TechApp.kt
+++ app/src/main/java/com/example/red30/compose/Red30TechApp.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.red30.R
@@ -33,7 +34,7 @@ fun Red30TechApp(modifier: Modifier = Modifier) {
             ) {
                 Image(
                     painter = painterResource(R.drawable.alternate_stacked_logo_color),
-                    contentDescription = "logo"
+                    contentDescription = stringResource(R.string.logo)
                 )
 
                 ElevatedButton(
