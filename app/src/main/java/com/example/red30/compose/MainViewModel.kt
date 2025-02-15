2dd0148bfe4e6e69f6c0f6852be1e0a5ec30fbe5
Annyce Davis <annyce.davis@gmail.com>
2025-02-15 13:27:41 -0500

    [+]: 06_03 start

--- app/src/main/java/com/example/red30/compose/MainViewModel.kt
+++ app/src/main/java/com/example/red30/compose/MainViewModel.kt
@@ -1,18 +1,24 @@
 package com.example.red30.compose
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.red30.data.ConferenceDataUiState
 import com.example.red30.data.ConferenceRepository
+import com.example.red30.data.getSelectedSession
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 private const val TAG = "MainViewModel"
 
 class MainViewModel(
+    private val savedStateHandle: SavedStateHandle,
     private val conferenceRepository: ConferenceRepository
 ) : ViewModel() {
 
@@ -20,6 +26,16 @@ class MainViewModel(
         ConferenceDataUiState(isLoading = true)
     )
     val uiState: StateFlow<ConferenceDataUiState> = _uiState
+
+    val selectedSession = savedStateHandle.getStateFlow<Int?>(
+        "sessionId", null
+    ).map { sessionId ->
+        sessionId?.let { _uiState.value.getSelectedSession(sessionId) }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Companion.WhileSubscribed(5000),
+        initialValue = null
+    )
 
     init {
         viewModelScope.launch {
@@ -32,5 +48,9 @@ class MainViewModel(
             }
             Log.i(TAG, "initialized: $sessionsInfos")
         }
+    }
+
+    fun setSelectedSessionId(sessionId: Int) {
+        savedStateHandle["sessionId"] = sessionId
     }
 }
