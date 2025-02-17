803bb4ab6c79d07b299342d9b787f2520956d7f7
Annyce Davis <annyce.davis@gmail.com>
2025-02-16 23:48:44 -0500

    [+]: 08_01 start

---
+++ app/src/screenshotTest/kotlin/com/example/red30/compose/ui/screen/SessionScreenShotTests.kt
@@ -0,0 +1,22 @@
+package com.example.red30.compose.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.example.red30.compose.ui.component.SessionItem
+import com.example.red30.compose.ui.theme.Red30TechTheme
+import com.example.red30.data.SessionInfo
+import com.example.red30.data.fake2
+
+@Suppress("unused")
+class SessionScreenShotTests {
+
+    @PreviewLightDark
+    @Composable
+    fun SessionItemPreview() {
+        Red30TechTheme {
+            SessionItem(
+                sessionInfo = SessionInfo.fake2()
+            )
+        }
+    }
+}
