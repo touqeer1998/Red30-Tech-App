b8a71b1afff1ad572a8d4e8935ba1479e1da86f3
Annyce Davis <annyce.davis@gmail.com>
2025-02-18 10:15:26 -0500

    [*]: 08_04 end

--- app/src/androidTest/java/com/example/red30/Red30TechAppTests.kt
+++ app/src/androidTest/java/com/example/red30/Red30TechAppTests.kt
@@ -3,12 +3,20 @@ package com.example.red30
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.semantics.ProgressBarRangeInfo
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasProgressBarRangeInfo
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.isOn
 import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
@@ -94,6 +102,22 @@ class Red30TechAppTests {
     }
 
     @Test
+    fun should_allow_session_to_be_favorited() {
+        composeRule.setContent {
+            Red30TechApp()
+        }
+
+        composeRule.apply {
+            onAllNodes(hasTestTag("ui:sessionItem"))
+                .onFirst()
+                .onChildren()
+                .filterToOne(hasClickAction())
+                .performClick()
+                .assert(isOn() and hasContentDescription("un-favorite session"))
+        }
+    }
+
+    @Test
     fun should_display_bottom_bar_when_compact() {
         onDevice().setDisplaySize(
             widthSizeClass = WidthSizeClass.COMPACT,
@@ -122,7 +146,6 @@ class Red30TechAppTests {
         composeRule
             .onNodeWithTag("ui:navigationRail").assertIsDisplayed()
     }
-
 
     @OptIn(ExperimentalTestApi::class)
     @Test
--- app/src/main/java/com/example/red30/compose/ui/screen/SessionsScreen.kt
+++ app/src/main/java/com/example/red30/compose/ui/screen/SessionsScreen.kt
@@ -15,7 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -73,7 +73,7 @@ fun SessionsList(
     onFavoriteClick: (sessionId: Int) -> Unit = {},
     onDayClick: (day: Day) -> Unit = {}
 ) {
-    var selectedChipIndex by remember { mutableIntStateOf(0) }
+    var selectedChipIndex by rememberSaveable { mutableIntStateOf(0) }
 
     LazyVerticalGrid(
         modifier = modifier
